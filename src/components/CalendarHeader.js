69176de9b78dff97dd636a9144a3839669c9f0f2
Taha <shahzadtaha638@gmail.com>
2023-06-09 20:02:55 +0500

    calender header created

--- src/components/CalendarHeader.js
+++ src/components/CalendarHeader.js
@@ -1,52 +1,83 @@
 import dayjs from "dayjs";
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import logo from "../assets/logo.png";
 import GlobalContext from "../context/GlobalContext";
 export default function CalendarHeader() {
-  const { monthIndex, setMonthIndex } = useContext(GlobalContext);
+  const { monthIndex, setMonthIndex} = useContext(GlobalContext);
+  const [years, setYears] = useState()
+
+  const [selectedMonthYear, setSelectedMonthYear] = useState(dayjs().format('MMM YYYY'))
+  const [selectedMonthYearIndex, setSelectedMonthYearIndex] = useState(dayjs().format('M/YYYY'))
+
   function handlePrevMonth() {
     setMonthIndex(monthIndex - 1);
+    const monthYear = dayjs().month(monthIndex - 1).format('MMM YY')
+    setSelectedMonthYear(monthYear)
   }
   function handleNextMonth() {
     setMonthIndex(monthIndex + 1);
+    const monthYear = dayjs().month(monthIndex + 1).format('MMM YY')
+    setSelectedMonthYear(monthYear)
   }
-  function handleReset() {
-    setMonthIndex(
-      monthIndex === dayjs().month()
-        ? monthIndex + Math.random()
-        : dayjs().month()
-    );
+
+  const getYearsList = () => {
+
+    const yearsList = []
+    const currentMonthYear = dayjs();
+    const numMonths = 11;
+
+    for (let i = 0; i < numMonths; i++) {
+      const monthYearText = currentMonthYear.add(i, 'month').format('MMM YY');
+      const monthNumeric = currentMonthYear.add(i, 'month').format('M');
+      const yearNumeric = currentMonthYear.add(i, 'month').format('YYYY');
+      yearsList.push({ text: monthYearText, monthIndex: monthNumeric, yearIndex: yearNumeric});
+    }
+
+    setYears(yearsList)
   }
+
+  const keyChange = (data) => {
+    setMonthIndex(parseInt(data?.monthIndex) -1);
+    setSelectedMonthYear(data?.text)
+  }
+
+  useEffect(() => {
+    getYearsList()
+  }, [])
+
   return (
+    <>
     <header className="px-4 py-2 flex items-center">
-      {/* <img src={logo} alt="calendar" className="mr-2 w-12 h-12" /> */}
-      {/* <h1 className="mr-10 text-xl text-gray-500 fond-bold">
-        Calendar
-      </h1> */}
-      <button
-        onClick={handleReset}
-        className="border rounded py-2 px-4 mr-5"
-      >
-        Today
-      </button>
+      
       <button onClick={handlePrevMonth}>
-        <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+        <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2 my-2">
           chevron_left
         </span>
       </button>
 
-      <h2 className="ml-4 text-xl text-gray-500 font-bold">
-        {dayjs(new Date(dayjs().year(), monthIndex)).format(
-          "MMMM YY"
-        )}
-      </h2>
-      
+      <div  className="w-full flex flex-row items-center text-center align-middle justify-between">
+
+      {years?.map(val => (
+
+        <h3 style={{color:'#44554a'}} onClick={() => keyChange(val)} className="ml-2 font-semibold mr-2 text-lg cursor-pointer">
+          {val?.text}
+        </h3>
+
+      ))
+      }
+      </div>
+
       <button onClick={handleNextMonth}>
-        <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2">
+        <span className="material-icons-outlined cursor-pointer text-gray-600 mx-2 my-2">
           chevron_right
         </span>
       </button>
-      
+
+
     </header>
+      <div style={{border:'1px solid #dac58c'}} className="w-full items-center text-center justify-center flex h-10 mt-2">
+        <p className="text-lg font-semibold" style={{color:'#44554a'}}> {selectedMonthYear ? selectedMonthYear : ""} </p>
+      </div>
+      </>
   );
 }
